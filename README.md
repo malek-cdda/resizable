bc623d45a19967c0ef87c3961d642450495ff978
abdul malek <143941431+malek-cdda@users.noreply.github.com>
2023-11-20 11:39:34 +0600

    resizing

--- README.md
+++ README.md
@@ -1,36 +1,6 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Resizable
 
-## Getting Started
+first we need to declare design, for this reason i create a div with 100px width and 100px height , the div border color is blue . i create four indicator sign , these sign are top-left , top-right , bottom-left, bottom-right stay.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<!--
+He realized that the fix for that bug is extremely simple, instead of applying the mousedown event on the resizer, he switched it to the whole window therefore if the mouse is somehow slipped out of the resizer again, the element will still resize base on the mouse position. He also added a mouseup event on the window object so that when he releases the mouse, the element will stop resizing. -->
--- src/app/resizer/page.tsx
+++ src/app/resizer/page.tsx
@@ -1,14 +1,136 @@
-import React from "react";
-import style from "./style.module.css";
+"use client";
+import React, { useEffect } from "react";
+// import style from "./style.module.css";
 const Home = () => {
+  function makeResizableDiv(div: any, div1: any) {
+    // console.log(window.innerWidth, window.innerHeight);
+    const mainDiv = document.getElementById("main-div");
+    const element = document.querySelector(div);
+    const resizers = document.querySelectorAll(div + div1);
+    const minimum_size = 20;
+    let original_width = 0;
+    let original_height = 0;
+    let original_x = 0;
+    let original_y = 0;
+    let original_mouse_x = 0;
+    let original_mouse_y = 0;
+    for (let i = 0; i < resizers.length; i++) {
+      const currentResizer = resizers[i];
+      currentResizer.addEventListener("mousedown", function (e) {
+        e.preventDefault();
+        original_width = parseFloat(
+          getComputedStyle(element, null)
+            .getPropertyValue("width")
+            .replace("px", "")
+        );
+        original_height = parseFloat(
+          getComputedStyle(element, null)
+            .getPropertyValue("height")
+            .replace("px", "")
+        );
+        original_x = element.getBoundingClientRect().left;
+        original_y = element.getBoundingClientRect().top;
+        original_mouse_x = e.pageX;
+        original_mouse_y = e.pageY;
+        window.addEventListener("mousemove", resize);
+        window.addEventListener("mouseup", stopResize);
+      });
+
+      function resize(e) {
+        if (currentResizer.classList.contains("bottom-right")) {
+          const width = original_width + (e.pageX - original_mouse_x);
+          const height = original_height + (e.pageY - original_mouse_y);
+          if (width > minimum_size) {
+            element.style.width = width + "px";
+          }
+          if (height > minimum_size) {
+            element.style.height = height + "px";
+          }
+        } else if (currentResizer.classList.contains("bottom-left")) {
+          const height = original_height + (e.pageY - original_mouse_y);
+          const width = original_width - (e.pageX - original_mouse_x);
+          if (height > minimum_size) {
+            element.style.height = height + "px";
+          }
+          if (width > minimum_size) {
+            element.style.width = width + "px";
+            element.style.left =
+              original_x + (e.pageX - original_mouse_x) + "px";
+          }
+        } else if (currentResizer.classList.contains("top-right")) {
+          const width = original_width + (e.pageX - original_mouse_x);
+          const height = original_height - (e.pageY - original_mouse_y);
+          if (width > minimum_size) {
+            element.style.width = width + "px";
+          }
+          if (height > minimum_size) {
+            element.style.height = height + "px";
+            element.style.top =
+              original_y + (e.pageY - original_mouse_y) + "px";
+          }
+        } else {
+          const width = original_width - (e.pageX - original_mouse_x);
+          const height = original_height - (e.pageY - original_mouse_y);
+          if (width > minimum_size) {
+            element.style.width = width + "px";
+            element.style.left =
+              original_x + (e.pageX - original_mouse_x) + "px";
+          }
+          if (height > minimum_size) {
+            element.style.height = height + "px";
+            element.style.top =
+              original_y + (e.pageY - original_mouse_y) + "px";
+          }
+        }
+      }
+
+      function stopResize() {
+        window.removeEventListener("mousemove", resize);
+      }
+    }
+  }
+
+  useEffect(() => {
+    for (
+      let i = 0;
+      i < document.getElementsByClassName("resizable").length;
+      i++
+    ) {
+      const newSize = document.getElementsByClassName("resizable")[i];
+      newSize.addEventListener("mousemove", function (e) {
+        console.log(i);
+        makeResizableDiv(`.resizable${i}`, ` .resizer${i}`);
+      });
+    }
+  }, []);
   return (
-    <div>
-      <div className={style.resizable}>
-        <div className={style.resizers}>
-          <div className={`${style.resizer} ${style.topLeft}`}></div>
-          <div className={`${style.resizer} ${style.topRight}`}></div>
-          <div className={`${style.resizer} ${style.bottomLeft}`}></div>
-          <div className={`${style.resizer} ${style.bottomRight}`}></div>
+    <div className="container mx-auto">
+      <div
+        className=" left-72 top-72 main-div relative w-96 h-96 border border-green-500"
+        id="main-div">
+        <div className=" resizable resizable0 w-32 h-32 border border-green-500 mx-5 absolute ">
+          <div className="resizers h-full w-full border-green-500 relative">
+            <div className=" top-left resizer0 cursor-nwse-resize absolute -top-0 -left-0 border-t border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer0 top-right cursor-nesw-resize absolute -top-0 -right-0 border-t border-r  border-blue-700 w-4 h-4"></div>
+            <div className="resizer0 bottom-left cursor-nesw-resize absolute -bottom-0 -left-0 border-b border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer0 bottom-right cursor-nwse-resize absolute -bottom-0 -right-0 border-b border-r  border-blue-700 w-4 h-4"></div>
+          </div>
+        </div>
+        <div className="resizable resizable1   border w-32 h-32 my-5 absolute ">
+          <div className="resizers h-full w-full border-green-500">
+            <div className=" top-left resizer1 cursor-nwse-resize absolute -top-0 -left-0 border-t border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer1 top-right cursor-nesw-resize absolute -top-0 -right-0 border-t border-r  border-blue-700 w-4 h-4"></div>
+            <div className="resizer1 bottom-left cursor-nesw-resize absolute -bottom-0 -left-0 border-b border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer1 bottom-right cursor-nwse-resize absolute -bottom-0 -right-0 border-b border-r  border-blue-700 w-4 h-4"></div>
+          </div>
+        </div>
+        <div className="resizable resizable2  border w-32 h-32 mx-5 absolute">
+          <div className="resizers h-full w-full border-green-500">
+            <div className=" top-left resizer2 cursor-nwse-resize absolute -top-0 -left-0 border-t border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer2 top-right cursor-nesw-resize absolute -top-0 -right-0 border-t border-r  border-blue-700 w-4 h-4"></div>
+            <div className="resizer2 bottom-left cursor-nesw-resize absolute -bottom-0 -left-0 border-b border-l  border-blue-700 w-4 h-4 "></div>
+            <div className="resizer2 bottom-right cursor-nwse-resize absolute -bottom-0 -right-0 border-b border-r  border-blue-700 w-4 h-4"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -16,3 +138,93 @@ const Home = () => {
 };
 
 export default Home;
+
+// function makeResizableDiv(div: any) {
+//   const element = document.querySelector(div);
+//   const resizers = document.querySelectorAll(div + " .resizer");
+//   const minimum_size = 20;
+//   let original_width = 0;
+//   let original_height = 0;
+//   let original_x = 0;
+//   let original_y = 0;
+//   let original_mouse_x = 0;
+//   let original_mouse_y = 0;
+//   for (let i = 0; i < resizers.length; i++) {
+//     const currentResizer = resizers[i];
+//     currentResizer.addEventListener("mousedown", function (e) {
+//       e.preventDefault();
+//       original_width = parseFloat(
+//         getComputedStyle(element, null)
+//           .getPropertyValue("width")
+//           .replace("px", "")
+//       );
+//       original_height = parseFloat(
+//         getComputedStyle(element, null)
+//           .getPropertyValue("height")
+//           .replace("px", "")
+//       );
+//       original_x = element.getBoundingClientRect().left;
+//       original_y = element.getBoundingClientRect().top;
+//       original_mouse_x = e.pageX;
+//       original_mouse_y = e.pageY;
+//       window.addEventListener("mousemove", resize);
+//       window.addEventListener("mouseup", stopResize);
+//     });
+
+//     function resize(e) {
+//       if (currentResizer.classList.contains("bottom-right")) {
+//         const width = original_width + (e.pageX - original_mouse_x);
+//         const height = original_height + (e.pageY - original_mouse_y);
+//         if (width > minimum_size) {
+//           element.style.width = width + "px";
+//         }
+//         if (height > minimum_size) {
+//           element.style.height = height + "px";
+//         }
+//       } else if (currentResizer.classList.contains("bottom-left")) {
+//         const height = original_height + (e.pageY - original_mouse_y);
+//         const width = original_width - (e.pageX - original_mouse_x);
+//         if (height > minimum_size) {
+//           element.style.height = height + "px";
+//         }
+//         if (width > minimum_size) {
+//           element.style.width = width + "px";
+//           element.style.left =
+//             original_x + (e.pageX - original_mouse_x) + "px";
+//         }
+//       } else if (currentResizer.classList.contains("top-right")) {
+//         const width = original_width + (e.pageX - original_mouse_x);
+//         const height = original_height - (e.pageY - original_mouse_y);
+//         if (width > minimum_size) {
+//           element.style.width = width + "px";
+//         }
+//         if (height > minimum_size) {
+//           element.style.height = height + "px";
+//           element.style.top =
+//             original_y + (e.pageY - original_mouse_y) + "px";
+//         }
+//       } else {
+//         const width = original_width - (e.pageX - original_mouse_x);
+//         const height = original_height - (e.pageY - original_mouse_y);
+//         if (width > minimum_size) {
+//           element.style.width = width + "px";
+//           element.style.left =
+//             original_x + (e.pageX - original_mouse_x) + "px";
+//         }
+//         if (height > minimum_size) {
+//           element.style.height = height + "px";
+//           element.style.top =
+//             original_y + (e.pageY - original_mouse_y) + "px";
+//         }
+//       }
+//     }
+
+//     function stopResize() {
+//       window.removeEventListener("mousemove", resize);
+//     }
+//   }
+// }
+
+// useEffect(() => {
+//   makeResizableDiv(".resizable");
+// }, []);
